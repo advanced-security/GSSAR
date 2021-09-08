948a93806f86dc4a6150937209d0a7b2db8f2e59
NickLiffen <nickliffen@gmail.com>
2021-09-08 13:37:00 +0100

    Pushing new Secret Function which sends data to EventBridge

--- functions/authorizers/githubWebhookSecretValidator/src/main.ts
+++ functions/authorizers/githubWebhookSecretValidator/src/main.ts
@@ -1,17 +1,37 @@
 import { ssm } from "./ssm";
-import { APIGatewayProxyResultV2 } from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import { secretVerifier } from "./verify";
 
+import {
+  EventBridgeClient,
+  PutEventsCommand,
+} from "@aws-sdk/client-eventbridge";
+
 export const handler = async (
-  event: any,
-  context: any
+  event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> => {
   console.log(event);
-  console.log(context);
+
   try {
     await ssm();
 
     const response = (await secretVerifier(event)) as boolean;
+
+    const client = new EventBridgeClient({ region: process.env.REGION });
+
+    const input = {
+      Entries: [
+        {
+          Source: "custom.kickOffSecretScanRemediation",
+          EventBusName: process.env.EVENT_BUS_NAME,
+          DetailType: "transaction",
+          Time: new Date(),
+          Detail: event.body,
+        },
+      ],
+    };
+
+    const command = new PutEventsCommand(input);
 
     if (!response)
       return {
@@ -19,9 +39,26 @@ export const handler = async (
         body: "Webhook secret provided does not match. unauthorized.",
       };
 
+    const { Entries } = await client.send(command);
+
+    console.log(Entries);
+
+    const checkKeyPresenceInArray = (key: string) => {
+      return (Entries) ? Entries.some((obj) => Object.keys(obj).includes(key)) : false;
+    };
+
+    const isKeyPresent = checkKeyPresenceInArray("ErrorCode");
+
+    if (isKeyPresent) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong. Please try again later.",
+      };
+    }
+
     return {
       statusCode: 200,
-      body: "Webhook secret provided does match. authorized.",
+      body: "Success! Message sent to Step Functions State Machine",
     };
   } catch (e: any) {
     const body = e.message || "";
